bfd708556f4ce7b3c68c2eab775a91418ac0c1ee
huzhangyang <huzhangyang@gmail.com>
2015-07-14 11:54:23 +0800

    a casually designed introscene

---
+++ Assets/Scripts/UI/ButtonClickEvent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonClickEvent: MonoBehaviour {
+
+	public void IntroScene_New()
+	{
+
+	}
+
+	public void IntroScene_Continue()
+	{
+
+	}
+
+	public void IntroScene_Setting()
+	{
+
+	}
+
+	public void IntroScene_Exit()
+	{
+		Application.Quit();
+	}
+}
