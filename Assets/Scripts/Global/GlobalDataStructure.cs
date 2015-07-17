6fd6ddea214415aa55baa88dd6ac46976b24bf54
huzhangyang <huzhangyang@gmail.com>
2015-07-17 14:49:53 +0800

    two casually designed scenes and more.

---
+++ Assets/Scripts/Global/GlobalDataStructure.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+管理全局数据结构,如常量,枚举,结构体等.
+*/
---
+++ Assets/Scripts/Global/GlobalManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalManager : MonoBehaviour {
+
+/*
+全局管理器
+*/
+
+	private static GlobalManager s_Instance;
+	public GlobalManager() { s_Instance = this; }
+	public static GlobalManager Instance { get { return s_Instance; } }
+	
+	void Start()
+	{
+		DontDestroyOnLoad(this.gameObject);
+		Application.runInBackground = false;
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+	}
+	
+	void OnApplicationFocus(bool focusd)
+	{
+		if (!focusd) 
+		{//进入后台
+
+		}
+		else
+		{//进入前台
+
+		}
+	}
+}
