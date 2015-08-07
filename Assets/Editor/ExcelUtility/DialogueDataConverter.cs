42120c79c4ed5ccbf595e5d39534610565009903
huzhangyang <huzhangyang@gmail.com>
2015-08-07 12:22:29 +0800

    implement DialogueDataConverter

---
+++ Assets/Editor/ExcelUtility/DialogueDataConverter.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using Excel;
+
+public class DialogueDataConvertor : MonoBehaviour
+{
+	const string PATH_EXCEL = "/Res/ExcelData/DialogueData.xls";
+	const string PATH_ASSET = "Assets/Resources/GameData/DialogueData.asset";
+	static DialogueDatas dialogueDatas;
+	static DataTable ExcelData;
+
+	[MenuItem("CRGTools/DialogueDataConverter")]
+	public static void ProcessConvert()
+	{		
+		FileStream stream = File.Open(Application.dataPath + PATH_EXCEL, FileMode.Open, FileAccess.Read);
+		IExcelDataReader excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
+		ExcelData = excelReader.AsDataSet ().Tables["Dialogue"];
+		excelReader.Dispose ();
+		dialogueDatas = ScriptableObject.CreateInstance<DialogueDatas>();
+		dialogueDatas.datas = new List<DialogueData>();
+		
+		if (ExcelData != null)
+		{
+			LoadData();
+			AssetDatabase.CreateAsset(dialogueDatas, PATH_ASSET);
+		}
+	}
+
+	static void LoadData()
+	{
+		DialogueData data = null;
+		
+		for (int i = 1; i < ExcelData.Rows.Count;)
+		{
+			int dialogueID = ExcelUtility.GetIntCell(ExcelData, i, 0);
+
+			data = new DialogueData(dialogueID);
+			dialogueDatas.datas.Add(data);
+
+			SentenceData sentence;
+			sentence.tellerName = ExcelUtility.GetCell(ExcelData, i, 1);
+			sentence.avatarID = ExcelUtility.GetIntCell(ExcelData, i, 2);
+			sentence.content = ExcelUtility.GetCell(ExcelData, i, 3);
+			data.sentences.Add(sentence);
+
+			while(++i < ExcelData.Rows.Count && ExcelUtility.GetIntCell(ExcelData, i, 0, true, 0) == 0)
+			{
+				sentence.tellerName = ExcelUtility.GetCell(ExcelData, i, 1);
+				sentence.avatarID = ExcelUtility.GetIntCell(ExcelData, i, 2);
+				sentence.content = ExcelUtility.GetCell(ExcelData, i, 3);
+				data.sentences.Add(sentence);
+			}
+		}
+	}
+	
+}
+
+
+
---
+++ Assets/Scripts/Data/DialogueDatas.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DialogueDatas : ScriptableObject 
+{
+	public List<DialogueData> datas;
+	
+	public DialogueData GetDialogueData(int dialogueID)
+	{
+		foreach (DialogueData data in datas)
+		{
+			if (data.dialogueID == dialogueID)
+				return data;
+		}
+		Debug.LogError("Asking for an non-exist dialogue:" + dialogueID);
+		return null;
+	}
+}
+
+[System.Serializable]
+public class DialogueData {
+/*
+ * 对话数据。
+*/
+	public int dialogueID;
+	public List<SentenceData> sentences;
+	public DialogueData(int dialogueID)
+	{
+		this.dialogueID = dialogueID;
+		sentences = new List<SentenceData> ();
+	}
+}
+
+[System.Serializable]
+public struct SentenceData {
+	public string tellerName;//讲话者姓名
+	public int avatarID;//讲话者头像ID
+	public string content;//讲话内容
+}
