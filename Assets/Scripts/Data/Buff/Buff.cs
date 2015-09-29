f6e9910d435b4d4969d66e1e199666d9bf145da3
huzhangyang <huzhangyang@gmail.com>
2015-09-29 16:33:20 +0800

    implements more buff framework.

--- Assets/Scripts/Data/Buff/Buff.cs
+++ Assets/Scripts/Data/Buff/Buff.cs
@@ -6,15 +6,73 @@ public class Buff {
 
 	BuffData data;
 	BuffEffect effect;
+	int effectTurns;
 
 	public Buff(BuffData data)
 	{
 		this.data = data;
+		this.effectTurns = data.lastTurns;
 		ParseBuffEffect(data.buffEffect);
+		OnAddBuff();
 	}
 
 	private void ParseBuffEffect(string effectString)
 	{
+		string name;
+		string[] args;
 
+		string[] splitedString = effectString.Split(':');
+		name = splitedString[0].Trim();
+		args = splitedString[1].Split(',');
+
+		if (name.Equals(""))
+		{
+			effect = new BuffEffectNone();
+		}
+		else
+		{
+			effect = new BuffEffectNone();
+		}
+	}
+	//check on every turn, see if buff still valid
+	public int Tick()
+	{
+		effectTurns--;
+		if(effectTurns <= 0)
+		{
+			effect.Revert();			
+		}
+		return effectTurns;
+	}
+
+	/*-----BUFF CALLBACK-----*/
+	public void OnAddBuff()
+	{
+		if(data.trigger == BuffTrigger.Always)
+			effect.Execute();
+	}
+
+	public void OnHit()
+	{
+		if(data.trigger == BuffTrigger.Hit)
+			effect.Execute();
+	}
+
+	public void OnBeHit()
+	{
+		if(data.trigger == BuffTrigger.Behit)
+			effect.Execute();
+	}
+
+	public void OnReady()
+	{
+		if(data.trigger == BuffTrigger.Ready)
+			effect.Execute();
+	}
+
+	public void OnAction()
+	{
+		if(data.trigger == BuffTrigger.Action)
+			effect.Execute();
 	}
 }
---
+++ Assets/Scripts/Data/Buff/BuffEffectNone.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class BuffEffectNone:BuffEffect 
+{
+	public override void Execute()
+	{
+	}
+
+	public override void Revert()
+	{
+	}
+}
