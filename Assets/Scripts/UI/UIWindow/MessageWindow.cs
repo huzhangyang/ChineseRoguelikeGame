73dd89fb803cda5085d7f352dbf888560d25e4c5
huzhangyang <huzhangyang@gmail.com>
2015-11-14 15:28:09 +0800

    fix text alighment.

--- Assets/Scripts/UI/UIWindow/MessageWindow.cs
+++ Assets/Scripts/UI/UIWindow/MessageWindow.cs
@@ -9,7 +9,8 @@ public class MessageWindow : MonoBehaviour {
 	
 	Text message;
 	List<string> msgList = new List<string>();
-	const int MAX_MSG_COUNT = 20;
+	const int MAX_MSG_COUNT = 25;
+	const int ALIGNMENT_MSG_COUNT = 15;
 	
 	void Awake()
 	{
@@ -36,18 +37,30 @@ public class MessageWindow : MonoBehaviour {
 	void OnMessageUpdate(MessageEventArgs args)
 	{
 		string msg = args.GetMessage<string>("Message");
+		//handle msgList
 		msgList.Add(msg);
 		while(msgList.Count > MAX_MSG_COUNT)
 		{
 			msgList.RemoveAt(0);
 		}
+		//handle alignment
+		if (msgList.Count >= ALIGNMENT_MSG_COUNT)
+		{
+			message.alignment = TextAnchor.LowerLeft;
+		} 
+		else
+		{
+			message.alignment = TextAnchor.UpperLeft;
+		}
+		//reassamble message
 		message.text = "";
 		for(int i = 0; i < msgList.Count - 1; i++)
 		{
 			message.text += msgList[i] + "\n";
 		}
+		//highlight latest message with animation
 		message.DOKill();
-		message.DOText(message.text + "<color=yellow>" + msgList[msgList.Count - 1] + "</color>", 1, true).SetEase(Ease.Linear);
+		message.DOText(message.text + "<color=yellow>" + msgList[msgList.Count - 1] + "</color>", 0.5f).SetEase(Ease.Linear);
 	}
 		
 	void ClearMessage()
